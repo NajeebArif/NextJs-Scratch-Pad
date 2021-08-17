cccc76cf2851c40b314f20dd84fbe2c82edf3f36
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-17 21:20:02 +0530

    Format Document

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,13 @@ function CompA() {
 
   const [value, setValue] = useState(0);
 
-
   return (
     <>
       <h2>Component A</h2>
       <div>
         <span>Current Value value: {value}</span>
-        <button onClick={() => setValue(value+1)}>+</button>
-        <button onClick={() => setValue(value-1)}>-</button>
+        <button onClick={() => setValue(value + 1)}>+</button>
+        <button onClick={() => setValue(value - 1)}>-</button>
       </div>
     </>
   )
