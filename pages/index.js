f59bd0b5f39b58d3605d8fa20f8720380bea68c7
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-17 21:16:00 +0530

    Change -> useState
    use setValue directly in onClick handler

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,14 @@ function CompA() {
 
   const [value, setValue] = useState(0);
 
-  const changeValue = (increamentor) => {
-    setValue(value + increamentor);
-  }
 
   return (
     <>
       <h2>Component A</h2>
       <div>
         <span>Current Value value: {value}</span>
-        <button onClick={() => changeValue(1)}>+</button>
-        <button onClick={() => changeValue(-1)}>-</button>
+        <button onClick={() => setValue(value+1)}>+</button>
+        <button onClick={() => setValue(value-1)}>-</button>
       </div>
     </>
   )
