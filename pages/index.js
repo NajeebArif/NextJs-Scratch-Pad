0947f82b4ca30021c2a8166ea427b51d3127404f
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-17 21:09:58 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
+
+function CompA(){
+
+  const [value, setValue] = useState(0);
+
+  const changeValue = (increamentor) =>{
+    setValue(value+increamentor);
+  }
+
+  return (
+    <>
+      <h2>Component A</h2>
+      <div>
+        <span>Current Value value: {value}</span>
+        <button onClick={()=>changeValue(1)}>+</button>
+        <button onClick={()=>changeValue(-1)}>-</button>
+      </div>
+    </>
+  )
+}
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <>
+      <h1>Home Page</h1>
+      <CompA />
+    </>
   )
 }
