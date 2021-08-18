06c7de7bd188ceea7f26f6e7cb947230b2f8b26a
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 22:55:59 +0530

    Update new.js
    Use ResourceForm component

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,125 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import axios from 'axios';
 import { useRouter } from "next/router";
-
-
-const DEFAULT_DATA = {
-    title: "",
-    description: "",
-    link: "",
-    priority: "2",
-    timeToFinish: 60
-}
+import ResourceForm from 'components/ResourceForm';
+import FormContainer from 'components/FormContainer';
 
 const ResourceCreate = () => {
 
-    const [form, setForm] = useState(DEFAULT_DATA);
     const router = useRouter();
 
-    const submitForm = () => {
-        axios.post("/api/resources", form)
+    const submitForm = formData => {
+        axios.post("/api/resources", formData)
             .then(_ => router.push("/"))
             .catch(err => alert(err?.response?.data));
     }
 
-    const resetForm = () => setForm(DEFAULT_DATA);
-
-    const handleChange = (e) => {
-        const [name, value] = e.target;
-        setForm({
-            ...form,
-            [name]: value
-        })
-    }
-
     return (
-        <div>
-            <div className="container">
-                <div className="columns">
-                    <div className="column is-8 is-offset-2">
-                        <div className="resource-form">
-                            <h1 className="title">Add New Resource</h1>
-                            <form>
-                                <div className="field">
-                                    <label className="label">Title</label>
-                                    <div className="control">
-                                        <input
-                                            value={form.title}
-                                            onChange={handleChange}
-                                            name="title"
-                                            className="input"
-                                            type="text"
-                                            placeholder="Learn Next JS and Sanity IO" />
-                                    </div>
-                                </div>
-
-                                <div className="field">
-                                    <label className="label">Description</label>
-                                    <div className="control">
-                                        <textarea
-                                            className="textarea"
-                                            value={form.description}
-                                            onChange={handleChange}
-                                            name="description"
-                                            placeholder="Learn these technologies because they are very popular and enable better SEO"></textarea>
-                                    </div>
-                                </div>
-
-                                <div className="field">
-                                    <label className="label">Link</label>
-                                    <div className="control">
-                                        <input
-                                            className="input"
-                                            value={form.link}
-                                            onChange={handleChange}
-                                            name="link"
-                                            type="text"
-                                            placeholder="https://academy.eincode.com" />
-                                    </div>
-                                </div>
-
-                                <div className="field">
-                                    <label className="label">Priority</label>
-                                    <div className="control">
-                                        <div className="select">
-                                            <select value={form.priority} onChange={handleChange}
-                                                name="priority">
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="field">
-                                    <label className="label">Time to finish</label>
-                                    <div className="control">
-                                        <input
-                                            className="input"
-                                            value={form.timeToFinish}
-                                            onChange={handleChange}
-                                            name="timeToFinish"
-                                            type="number"
-                                            placeholder="60 (time is in minutes)" />
-                                    </div>
-                                    <p className="help">Time is in minutes</p>
-                                </div>
-
-                                <div className="field is-grouped">
-                                    <div className="control">
-                                        <button type="button" onClick={submitForm} className="button is-link">Submit</button>
-                                    </div>
-                                    <div className="control">
-                                        <button type="button" onClick={resetForm} className="button is-link is-light">Reset</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <FormContainer>
+            <ResourceForm
+                onFormSubmit={submitForm}
+            />
+        </FormContainer>
     )
 }
 
