64b156a1ea4134bac2bef31c002ae0ad53828907
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 20:02:00 +0530

    Create [id].js

---
+++ pages/resources/[id].js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function ResourceDetail({resource}) {
+    return (
+        <div>
+            <section className="hero ">
+                <div className="hero-body">
+                    <div className="container">
+                        <section className="section">
+                            <div className="columns">
+                                <div className="column is-8 is-offset-2">
+                                    <div className="content is-medium">
+                                        <h2 className="subtitle is-4">{resource.createdAt}</h2>
+                                        <h1 className="title">{resource.title}</h1>
+                                        <p>{resource.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export async function getServerSideProps({params}){
+    const resData = await fetch(`http://localhost:3001/api/resources/${params.id}`)
+    const data = await resData.json();
+    return {
+        props: {
+            resource: data
+        }
+    }
+}
