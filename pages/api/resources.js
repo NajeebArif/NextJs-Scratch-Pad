25efe8df41136ca6066c1098e56dc227d0385a84
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 17:15:14 +0530

    Update resources.js
    use await to fetch response data from axios

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default async function (req, res) {
     if (req.method === "GET") {
-        const dataRes = await fetch('http://localhost:3001/api/resources');
+        const dataRes = await fetch("http://localhost:3001/api/resources");
         const data = await dataRes.json();
         return res.send(data)
     }
@@ -12,7 +12,11 @@ export default async function (req, res) {
         if (!title || !description || !link || !timeToFinish || !priority) {
             return res.status(422).send("Data are missing!");
         }
-        axios.post("http://localhost:3001/api/resources",req.body);
-        return res.send("Request recieved.")
+        try{
+            const axiosResponse = await axios.post("http://localhost:3001/api/resources",req.body);
+            return res.send(axiosResponse.data);
+        }catch {
+            return res.status(422).send("Data can not be saved");
+        }
     }
 }
