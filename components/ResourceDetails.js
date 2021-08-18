782cde5cab346cc71a041df51df0daca3f243ae8
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 20:10:42 +0530

    Update ResourceDetails.js
    refactor resource details

--- components/ResourceDetails.js
+++ components/ResourceDetails.js
@@ -1,26 +1,25 @@
 import React from 'react'
 
 export default function ResourceList({ resources }) {
+    const renderResources = () => resources.map(res =>
+    (<div key={res.id} className="column is-5 is-offset-1 ">
+        <div className="content is-medium">
+            <h2 className="subtitle is-5 has-text-grey">{res.createdAt}</h2>
+            <h1 className="title has-text-black is-3">{res.title}</h1>
+            <p className="has-text-dark">{res.description}</p>
+        </div>
+    </div>));
+
     return (
         <div>
             <section className="hero ">
                 <div className="hero-body">
                     <div className="container">
-                        {
-                            resources.map(res =>
-                                (<section key={res.id} className="section">
-                                    <div className="columns is-variable is-8">
-                                        <div className="column is-5 is-offset-1 ">
-                                            <div className="content is-medium">
-                                                <h2 className="subtitle is-5 has-text-grey">{res.createdAt}</h2>
-                                                <h1 className="title has-text-black is-3">{res.title}</h1>
-                                                <p className="has-text-dark">{res.description}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </section>)
-                            )
-                        }
+                        <div className="columns is-multiline is-variable is-8">
+                            {
+                                renderResources()
+                            }
+                        </div>
                     </div>
                 </div>
             </section>
