0cc315f084847d0ead33bca10c22f3dc08a1ab45
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 23:38:23 +0530

    Update index.js
    Add handler to activate the resource

--- pages/resources/[id]/index.js
+++ pages/resources/[id]/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import Link from 'next/link';
+import axios from "axios";
 
-export default function ResourceDetail({resource}) {
-    
+export default function ResourceDetail({ resource }) {
+
+    const activeResource = () => {
+        axios.patch("/api/resources", { ...resource, status: "active" })
+            .then(_ => alert("Resource has been activated!"))
+            .catch(_ => alert("Cannot active the resource!"))
+    }
+
     return (
         <div>
             <section className="hero ">
@@ -15,11 +22,17 @@ export default function ResourceDetail({resource}) {
                                         <h2 className="subtitle is-4">{resource.createdAt}</h2>
                                         <h1 className="title">{resource.title}</h1>
                                         <p>{resource.description}</p>
+                                        <p>Time to finish: {resource.timeToFinish} min</p>
                                         <Link href={`/resources/${resource.id}/edit`}>
-                                            <a className="button is-warning"> 
+                                            <a className="button is-warning">
                                                 Edit Resource
                                             </a>
                                         </Link>
+                                        <button
+                                            onClick={activeResource}
+                                            className="button is-success ml-1">
+                                            Activate
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -31,7 +44,7 @@ export default function ResourceDetail({resource}) {
     )
 }
 
-export async function getServerSideProps({params}){
+export async function getServerSideProps({ params }) {
     const resData = await fetch(`http://localhost:3001/api/resources/${params.id}`)
     const data = await resData.json();
     return {
