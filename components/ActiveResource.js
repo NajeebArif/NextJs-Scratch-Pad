7f779413cbf5a0443785bda10e64a62f2aba2cc5
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-19 00:26:08 +0530

    Update ActiveResource.js
    Add moment logic to calculate elapsed time

--- components/ActiveResource.js
+++ components/ActiveResource.js
@@ -1,36 +1,44 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import moment from "moment";
 
 const ActiveResource = () => {
 
     const [resource, setResource] = useState({});
 
-  useEffect(() => {
-    async function fetchResource() {
-      const axiosRes = await axios.get("/api/activeresource");
-      const resource = axiosRes.data;
-      setResource(resource);
-    }
+    useEffect(() => {
+        async function fetchResource() {
+            const axiosRes = await axios.get("/api/activeresource");
+            const resource = axiosRes.data;
+            const timeToFinish = parseInt(resource.timeToFinish, 10);
+            const elapsedTime = moment().diff(moment(resource.activationTime), "seconds");
+            const updatedTimeToFinish = (timeToFinish * 60) - elapsedTime;
 
-    fetchResource();
-  }, [])
+            if (updatedTimeToFinish >= 0) {
+                resource.timeToFinish = updatedTimeToFinish;
+            }
+            setResource(resource);
+        }
 
-  return (
-    <div className="active-resource">
-      <h1 className="resource-name">{resource.title}</h1>
-      <div className="time-wrapper">
-        <h2 className="elapsed-time">
-          1400
-        </h2>
-      </div>
-      <Link href="/">
-        <a className="button">
-          Go to resource
-        </a>
-      </Link>
-    </div>
-  )
+        fetchResource();
+    }, [])
+
+    return (
+        <div className="active-resource">
+            <h1 className="resource-name">{resource.title}</h1>
+            <div className="time-wrapper">
+                <h2 className="elapsed-time">
+                    {resource.timeToFinish}
+                </h2>
+            </div>
+            <Link href="/">
+                <a className="button">
+                    Go to resource
+                </a>
+            </Link>
+        </div>
+    )
 }
 
 export default ActiveResource;
