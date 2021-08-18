d9f93bb51cdd9b44dc68abc7e4fed29cd51b61bc
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 23:32:28 +0530

    Update Layout.js
    Use Active Resource in layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import ActiveResource from "components/ActiveResource";
 
 export default function Layout({children}) {
     return (
         <div>
             <Navbar />
+            <ActiveResource />
             {children}
         </div>
     )
