a8b43735e6a73da5bad257065783999bcfc466eb
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 23:20:52 +0530

    Update resources.js
    Handle patch request

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -2,20 +2,29 @@ import axios from "axios";
 
 
 export default async function (req, res) {
+    
     if (req.method === "GET") {
         const dataRes = await fetch("http://localhost:3001/api/resources");
         const data = await dataRes.json();
         return res.send(data)
     }
-    if (req.method === "POST") {
-        const { title, description, link, timeToFinish, priority } = req.body;
+
+    if (req.method === "POST" || req.method === "PATCH") {
+        
+        const { id, title, description, link, timeToFinish, priority } = req.body;
+        
         if (!title || !description || !link || !timeToFinish || !priority) {
             return res.status(422).send("Data are missing!");
         }
-        try{
-            const axiosResponse = await axios.post("http://localhost:3001/api/resources",req.body);
+
+        const url = req.method === "POST"
+            ? "http://localhost:3001/api/resources"
+            : `http://localhost:3001/api/resources/${id}`
+        
+            try {
+            const axiosResponse = await axios[req.method.toLowerCase()](url, req.body);
             return res.send(axiosResponse.data);
-        }catch {
+        } catch {
             return res.status(422).send("Data can not be saved");
         }
     }
