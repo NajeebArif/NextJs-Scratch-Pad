0a4a4d04e1caf4886fdf9452be18f2ff23a77430
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 16:52:18 +0530

    Update resources.js
    Add axios to proxy request to backend server

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,16 +1,18 @@
+import axios from "axios";
 
 
-export default async function(req,res){
-    if(req.method==="GET"){
+export default async function (req, res) {
+    if (req.method === "GET") {
         const dataRes = await fetch('http://localhost:3001/api/resources');
         const data = await dataRes.json();
         return res.send(data)
     }
-    if(req.method==="POST"){
-        const {title,description,link,timeToFinish,priority} = req.body;
+    if (req.method === "POST") {
+        const { title, description, link, timeToFinish, priority } = req.body;
         if (!title || !description || !link || !timeToFinish || !priority) {
             return res.status(422).send("Data are missing!");
-          }
+        }
+        axios.post("http://localhost:3001/api/resources",req.body);
         return res.send("Request recieved.")
     }
 }
