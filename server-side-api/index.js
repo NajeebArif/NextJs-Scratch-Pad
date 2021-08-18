c6c9d19d26297bed7b033d6ef58f68416f1ae770
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 14:25:30 +0530

    Update index.js
    update code to read data.json using path and fs node modules

--- server-side-api/index.js
+++ server-side-api/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express();
-const PORT = 3001
+const PORT = 3001;
+const fs = require('fs')
+const path = require('path')
+const pathToFile = path.resolve('./data.json')
+
+const getResources =()=> JSON.parse(fs.readFileSync(pathToFile))
+
+app.get('/api/resources',(req,res)=>{
+    res.send(getResources())
+})
 
 
 app.listen(PORT, ()=>{
