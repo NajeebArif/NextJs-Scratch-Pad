63f7c05e428aa4941590c38de485dae97ca002f1
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 12:03:35 +0530

    Update ResourceHighlight.js
    Use props.resources

--- components/ResourceHighlight.js
+++ components/ResourceHighlight.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function ResourceHighlight() {
+export default function ResourceHighlight({ resources }) {
     return (
         <>
             <section className="hero ">
@@ -13,36 +13,23 @@ export default function ResourceHighlight() {
                                 </figure>
                             </div>
                         </div>
-
-                        <section className="section">
-                            <div className="columns">
-                                <div className="column is-8 is-offset-2">
-                                    <div className="content is-medium">
-                                        <h2 className="subtitle is-4">December 25, 2018</h2>
-                                        <h1 className="title">Getting Started</h1>
-                                        <p>This is a starter template for creating a beautiful, customizable blog with minimal
-                                            effort. You’ll only have to change a few settings and you’re ready to go. As with all Jigsaw sites,
-                                            configuration settings can be found in config</p>
+                        {resources.map(resource => {
+                            return (
+                                <section key = {resource.id} className="section">
+                                    <div className="columns">
+                                        <div className="column is-8 is-offset-2">
+                                            <div className="content is-medium">
+                                                <h2 className="subtitle is-4">December 25, 2018</h2>
+                                                <h1 className="title">Getting Started</h1>
+                                                <p>This is a starter template for creating a beautiful, customizable blog with minimal
+                                                    effort. You’ll only have to change a few settings and you’re ready to go. As with all Jigsaw sites,
+                                                    configuration settings can be found in config</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        </section>
-
-                        <div className="is-divider"></div>
-
-                        <section className="section">
-                            <div className="columns">
-                                <div className="column is-8 is-offset-2">
-                                    <div className="content is-medium">
-                                        <h2 className="subtitle is-4">December 25, 2018</h2>
-                                        <h1 className="title">Getting Started</h1>
-                                        <p>This is a starter template for creating a beautiful, customizable blog with minimal
-                                            effort. You’ll only have to change a few settings and you’re ready to go. As with all Jigsaw sites,
-                                            configuration settings can be found in config</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
+                                </section>
+                            )
+                        })}
                     </div>
                 </div>
             </section>
