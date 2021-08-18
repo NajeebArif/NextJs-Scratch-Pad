44ed01ca5bc6dcc43a3602fdb8b2e4a5ac4df072
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-18 20:02:16 +0530

    Update ResourceHighlight.js
    Refactor

--- components/ResourceHighlight.js
+++ components/ResourceHighlight.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function ResourceHighlight({ resources }) {
     return (
@@ -6,31 +7,62 @@ export default function ResourceHighlight({ resources }) {
             <section className="hero ">
                 <div className="hero-body">
                     <div className="container">
-                        <div className="columns">
-                            <div className="column is-8 is-offset-2">
-                                <figure className="image is-16by9">
-                                    <img src="https://media.istockphoto.com/photos/bloggingblog-concepts-ideas-with-worktable-picture-id922745190?k=6&m=922745190&s=612x612&w=0&h=qBmau_GKQZasMW14NJBp3u2vU-f5YFCcN1N71Exa8iE=" alt="" />
-                                </figure>
-                            </div>
-                        </div>
-                        {resources.map(resource => {
-                            return (
-                                <section key = {resource.id} className="section">
-                                    <div className="columns">
-                                        <div className="column is-8 is-offset-2">
-                                            <div className="content is-medium">
-                                                <h2 className="subtitle is-4">{resource.createdAt}</h2>
-                                                <h1 className="title">{resource.title}</h1>
-                                                <p>{resource.description}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </section>
-                            )
-                        })}
+                        <ResourceHighlightBanner />
+                        {resources.map(resource => <ResourceSection key={resource.id} resource={resource} />)}
                     </div>
                 </div>
             </section>
         </>
+    )
+}
+
+const ResourceHighlightBanner = () => (
+    <div className="columns">
+        <div className="column is-8 is-offset-2">
+            <figure className="image is-16by9">
+                <img src="https://media.istockphoto.com/photos/bloggingblog-concepts-ideas-with-worktable-picture-id922745190?k=6&m=922745190&s=612x612&w=0&h=qBmau_GKQZasMW14NJBp3u2vU-f5YFCcN1N71Exa8iE=" alt="" />
+            </figure>
+        </div>
+    </div>
+)
+
+const ResourceSection = ({ resource }) => {
+    return (
+        <>
+            <section className="section">
+                <div className="columns">
+                    <div className="column is-8 is-offset-2">
+                        <div className="content is-medium">
+                            <ResourceContent
+                                createdAt={resource.createdAt}
+                                title={resource.title}
+                                description={resource.description}
+                            />
+                        </div>
+                        <DetailsButton id={resource.id} />
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+const ResourceContent = ({ createdAt, title, description }) => {
+    return (
+        <>
+            <h2 className="subtitle is-4">{createdAt}</h2>
+            <h1 className="title">{title}</h1>
+            <p>{description}</p>
+        </>
+    )
+}
+
+const DetailsButton = ({ id }) => {
+    return (
+        <Link href={`resources/${id}`}>
+            <button className="button is-primary">
+                Details
+            </button>
+        </Link>
     )
 }
